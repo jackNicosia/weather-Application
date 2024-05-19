c2d22d86845192903d7bd6ee39db5965ba2151da
Jack <jackrileynicosia@yahoo.com>
2024-05-19 18:31:42 -0400

    ran build

--- src/search.js
+++ src/search.js
@@ -20,7 +20,31 @@ export function performSearch() {
          document.getElementById('temp').innerHTML = response.current.temp_f + " °F";
          document.getElementById('condition').innerHTML = response.current.condition.text;
          console.log(response.forecast.forecastday[2].day.condition.text);
-      
+
+
+
+/* add rest of logic for search
+         const feelsLike = data.current.feelslike_f;
+         const condition = data.current.condition.text;
+         const locationName = data.location.name;
+         const locationRegion = data.location.region;
+         const windMph = data.current.wind_mph;
+         const minTemp = data.forecast.forecastday[0].day.mintemp_f;
+         const maxTemp = data.forecast.forecastday[0].day.maxtemp_f;
+         const snow = data.forecast.forecastday[0].day.daily_chance_of_snow;
+         const rain = data.forecast.forecastday[0].day.daily_chance_of_rain;
+         const img1 = data.current.condition.icon;
+         const date = data.current.last_updated;
+
+         document.getElementById("feelsLike").innerHTML = "Feels like " + feelsLike + " °F"
+         document.getElementById("wind").innerHTML = windMph + " Mph";
+         document.getElementById("minTemp").innerHTML = "Low " + minTemp + " °F";
+         document.getElementById("maxTemp").innerHTML = "High " + maxTemp + " °F";
+         document.getElementById("snow").innerHTML = "Snow " + snow + "%";
+         document.getElementById("rain").innerHTML = "Rain " + rain + "%";
+         document.getElementById("date").innerHTML = formattedTime;
+         document.getElementById("img1").src = "https:" + img1;
+      */
 
        })
        .catch(function(err) {
